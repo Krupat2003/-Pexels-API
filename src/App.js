7d00199c2d6368ba64acd021887f11fbeddd1a48
Krupa Timbadiya <krupatimbadiya1@gmail.com>
2023-09-20 16:22:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'bootswatch/dist/materia/bootstrap.min.css';
+import Search from './component/Search';
+
+function App() {
+  return (
+    <>
+    <div className="container mt-5">
+      <Search />
+    </div>
+
+    </>
+  );
+}
+
+export default App;
